4ddbadd196fb8c700fca6c9f5e2d30811897c7a8
nguetcheu <dnguetcheu@gmail.com>
2024-11-01 01:45:51 +0100

    Ajout du support du binding pour affichage et la liaison entre la classe xml et java

--- app/src/main/java/com/example/superquiz/WelcomeFragment.java
+++ app/src/main/java/com/example/superquiz/WelcomeFragment.java
@@ -8,12 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.superquiz.databinding.FragmentWelcomeBinding;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link WelcomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class WelcomeFragment extends Fragment {
+
+    private FragmentWelcomeBinding binding;
+
     public static WelcomeFragment newInstance() {
         WelcomeFragment fragment = new WelcomeFragment();
         return fragment;
@@ -24,10 +29,12 @@ public class WelcomeFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    // Ajout du support du binding pour affichage et la liaison entre la classe xml et java
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
