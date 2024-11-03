888b30d64b1437163ade65a8c62e5ef8284ee806
nguetcheu <dnguetcheu@gmail.com>
2024-11-03 01:55:55 +0100

    projet terminé

--- app/src/main/java/com/example/superquiz/Injection/ViewModelFactory.java
+++ app/src/main/java/com/example/superquiz/Injection/ViewModelFactory.java
@@ -1,2 +1,40 @@
-package com.example.superquiz.Injection;public class ViewModelFactory {
+package com.example.superquiz.Injection;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.superquiz.Data.QuestionBank;
+import com.example.superquiz.Data.QuestionRepository;
+import com.example.superquiz.Ui.quiz.QuizViewModel;
+
+import org.jetbrains.annotations.NotNull;
+
+public class ViewModelFactory implements ViewModelProvider.Factory {
+    private final QuestionRepository questionRepository;
+    private static ViewModelFactory factory;
+
+    public static ViewModelFactory getInstance() {
+        if (factory == null) {
+            synchronized (ViewModelFactory.class) {
+                if (factory == null) {
+                    factory = new ViewModelFactory();
+                }
+            }
+        }
+        return factory;
+    }
+
+    private ViewModelFactory() {
+        QuestionBank questionBank = QuestionBank.getInstance();
+        this.questionRepository = new QuestionRepository(questionBank);
+    }
+
+    @Override
+    @NotNull
+    public <T extends ViewModel>  T create(Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(QuizViewModel.class)) {
+            return (T) new QuizViewModel(questionRepository);
+        }
+        throw new IllegalArgumentException("Unknown ViewModel class");
+    }
 }
